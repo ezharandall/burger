03b9357df8c2887e91a1c629927c6bd79a3b2d2c
Ezha Randall <ezha.randall@gmail.com>
2017-10-24 14:33:25 -0400

    adding database files, edited code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Thick & Hearty');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
